e26086a9831a6ff430e4bebfedaf06460417158a
Leonardo Davalos <32350741+leothecrz@users.noreply.github.com>
2023-11-29 16:47:49 -0800

    divide differences table

---
+++ polynomial/src/main/java/cpp/polynomialinterpolation/Main.java
@@ -0,0 +1,138 @@
+package cpp.polynomialinterpolation;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Main {    
+
+    public static void main(String[] args) {
+
+        if(args.length > 0)
+        {
+            System.out.println("Args Error");
+            return;
+        }
+
+        try 
+        {
+
+            System.out.println("Enter the path to the text file with x and f(x) values: ");
+            System.out.print(" > ");
+            BufferedReader reader = new BufferedReader(new java.io.InputStreamReader(System.in) );
+            String filePath = reader.readLine();
+            double[][] data = readDataFromFile(filePath);
+
+            if (data != null)
+            {
+                double[] xValues = data[0];
+                double[] fxValues = data[1];
+                double[][] differenceTable = dividedDifferenceTable(xValues, fxValues);
+
+                double[] newtonCoefficients = Arrays.copyOf(differenceTable[0], differenceTable[0].length);
+                
+                System.out.println();
+                printMatrix(differenceTable);
+                System.out.println();
+                printLeftTopHalf(differenceTable);
+                System.out.println();
+
+                // System.out.println("\n Newton's Form Polynomial: ");
+                // printNewtonPolynomial();
+
+                // System.out.println("\n Lagrange's Form Polynomial: ");
+                // printLagrangePolynomial();
+
+                // System.out.println("\n Simplified Polynomial: ");
+                // printSimplifiedPolynomial();
+
+            }
+
+        } 
+        catch (IOException e) 
+        {
+            System.err.println();
+            System.err.println("Error: " + e.getMessage());
+            System.err.println();
+        }
+
+    }
+
+    
+
+    private static void printMatrix(double[][] matrix) 
+    {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printLeftTopHalf(double[][] matrix) 
+    {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        for (int i = 0; i < rows; i++) 
+        {
+            // Print elements up to the diagonal for the current row
+            for (int j = 0; j < cols - i; j++) 
+                System.out.print(matrix[i][j] + " ");
+            System.out.println(); // Move to the next line after printing each row
+        }
+    }
+
+    private static double[][] readDataFromFile(String filePath) throws IOException 
+    {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) 
+        {
+            double[] xVals = null;
+            double[] fxVals = null;
+
+            String line;
+            String[] splits = null;
+            //XVals
+            if ( ( line = reader.readLine()) != null ) 
+            {    
+                splits = line.split("\\s+");
+                xVals = Arrays.stream(splits).mapToDouble(Double::parseDouble).toArray();
+            }
+            else
+                throw new IOException("Missing X Values on Row(1)");
+
+            //FxVals
+            if ( ( line = reader.readLine()) != null ) 
+            {
+                splits = line.split("\\s+");
+                fxVals = Arrays.stream(splits).mapToDouble(Double::parseDouble).toArray();
+            }
+            else
+                throw new IOException("Missing F(x) Values on Row(2)");
+
+            return new double[][]{xVals,fxVals};
+        }
+    }
+
+    private static double[][] dividedDifferenceTable(double[] x, double[] fx) 
+    {
+        int n = x.length;
+        double[][] table = new double[n][n];
+
+        for (int i = 0; i < n; i++) 
+            table[i][0] = fx[i];
+        for (int j = 1; j < n; j++) 
+            for (int i = 0; i < n - j; i++) 
+                table[i][j] = (table[i + 1][j - 1] - table[i][j - 1]) / (x[i + j] - x[i]);
+        
+        return table;
+    }
+
+    
+
+    
+
+}
+
